a2c6e2bc94f59069a78ab03ff44b71d164aca6dc
Azad-786 <azadvocalss@gmail.com>
2022-12-05 13:27:35 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/header/Header';
+import MovieList from './components/movieList/movieList';
+import Home from './pages/home/Home';
+import Movie from './pages/home/movieDetail/Movie';
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+     <Header/>
+      <Routes>
+        <Route index element = {<Home/>}></Route>
+        <Route path='movie/:id' element ={<Movie/>}></Route>
+        <Route path='movies/:type' element={<MovieList/>}></Route>
+        <Route path="/*" element={<h1>Error Page</h1>}></Route>
+      </Routes>
+     </Router>  
+    </div>
+  );
+}
+
+export default App;
